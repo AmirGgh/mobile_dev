36fb1327df98a2a68734ccd60a50287acd99b4a9
AmirGgh <am.g.gith@gmail.com>
2026-04-29 20:23:00 +0300

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,14 @@ const expoConfig = require('eslint-config-expo/flat');
 module.exports = [
   ...expoConfig,
   {
+    languageOptions: {
+      globals: {
+        __dirname: 'readonly',
+        module: 'readonly',
+        require: 'readonly',
+        process: 'readonly',
+      },
+    },
     rules: {
       'import/no-unresolved': 'off',
       'react/no-unescaped-entities': 'off',
